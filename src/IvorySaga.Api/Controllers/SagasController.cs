dd47315e455eed011c2af65bf5c60e6b06913a53
Nathan Le Sommer <lesommer.nathan@gmail.com>
2021-02-15 19:49:00 +0100

    Add MediatR and AutoMapper (#3)
    * Add MediatR and AutoMapper
    
    + Clean projects
    
    * Switch projects to .NET 5

---
+++ src/IvorySaga.Api/Controllers/SagasController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using IvorySaga.Api.Models;
+using IvorySaga.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IvorySaga.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/sagas")]
+    public class SagasController : ControllerBase
+    {
+        private readonly ILogger<SagasController> _logger;
+        private readonly IMapper _mapper;
+        private readonly ISender _sender;
+
+        public SagasController(ILogger<SagasController> logger, IMapper mapper, ISender sender)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _sender = sender;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<SagaModel>>> GetAssetAsync(CancellationToken cancellationToken)
+        {
+            var query = new GetSagasQuery();
+            var response = await _sender.Send(query, cancellationToken);
+            return Ok(_mapper.Map<IReadOnlyList<SagaModel>>(response));
+        }
+    }
+}
---
+++ src/IvorySaga/Data/Saga.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IvorySaga.Data
+{
+    /// <summary>
+    /// Represents a Saga.
+    /// </summary>
+    public sealed class Saga
+    {
+        public Guid Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
